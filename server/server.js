e1349b55971129a460a590d84de7c6ce22d8bade
Aidan LeMay <aidanpathways12@gmail.com>
2022-04-21 21:52:04 -0400

    Finishing Login and Adding Token Auth to Endpoints

--- server/server.js
+++ server/server.js
@@ -4,8 +4,10 @@ const port = 3000;
 require("dotenv").config();
 
 // Route Definitions
+const auth = require("./authentication/auth");
 const register = require("./authentication/register");
 const login = require("./authentication/login");
+
 const rentals = require("./api/rentals/rentals");
 const inventory = require("./api/inventory/inventory");
 const customers = require("./api/customers/customers");
@@ -24,22 +26,21 @@ app.get("/", (req, res) => {
 app.post("/register", register);
 app.post("/login", login);
 
-app.get("/rentals", rentals);
-app.post("/rentals", rentals);
+app.get("/rentals", auth, rentals);
+app.post("/rentals", auth, rentals);
 
-app.get("/inventory", inventory);
-app.post("/inventory", inventory);
-app.put("/inventory", inventory);
-app.delete("/inventory", inventory);
+app.get("/inventory", auth, inventory);
+app.post("/inventory", auth, inventory);
+app.put("/inventory", auth, inventory);
+app.delete("/inventory", auth, inventory);
 
-app.get("/customers", customers);
+app.get("/customers", auth, customers);
 
 /* Error handler middleware */
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   console.error(err.message, err.stack);
   res.status(statusCode).json({ message: err.message });
-  return;
 });
 
 app.listen(port, () => {
