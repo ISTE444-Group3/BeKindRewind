b4824ae0d64bf683d369cda896babe3f48c0d8a0
Aidan LeMay <aidanpathways12@gmail.com>
2022-04-26 21:21:11 -0400

    Fixing CORS

--- server/server.js
+++ server/server.js
@@ -24,19 +24,8 @@ app.get("/", (req, res) => {
 });
 
 // CORS Definitions
-const allowedOrigins = ['http://localhost:3000', 'http://localhost:4200'];
-
 app.use(cors({
-  origin: function(origin, callback){    // allow requests with no origin 
-    // (like mobile apps or curl requests)
-    if(!origin) {
-      return callback(null, true);
-    }
-    if(allowedOrigins.indexOf(origin) === -1){
-      var msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }    return callback(null, true);
-  }
+  origin: '*'
 }));
 
 // Path Definitions
